05493cc0392568800679539a64043f15835f603e
Ivan Martinović <martinovic1412@gmail.com>
2022-07-24 21:44:42 +0200

    Initial commit

---
+++ DemoMNIST/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoMNIST
+//
+//  Created by Ivan Martinović on 24.07.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
